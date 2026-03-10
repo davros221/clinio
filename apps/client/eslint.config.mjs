a35c03450e9196abadbabb56e7ff29339ad20a41
davpu <david.pucciarelli@unicornuniversity.com>
2026-03-10 17:25:36 +0100

    CR update

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,28 +1,23 @@
 import nx from "@nx/eslint-plugin";
 import baseConfig from "../../eslint.config.mjs";
-// import js from "@eslint/js";
-// import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-// import globals from "globals";
 
 export default [
   ...baseConfig,
   ...nx.configs["flat/react"],
   {
-    ignores: ["dist"],
-  },
-  // js.configs.recommended,
-  // ...tseslint.configs.recommended,
-  {
     files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    settings: {
+      react: { version: "detect" }, // Automatically detect React version
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -31,5 +26,8 @@ export default [
         { allowConstantExport: true },
       ],
     },
+  },
+  {
+    ignores: ["dist", "coverage"],
   },
 ];
