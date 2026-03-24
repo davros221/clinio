af11cfa6bbf89b915f9d9d5c2d9ffea6b41deba7
davpu <david.pucciarelli@unicornuniversity.com>
2026-03-24 21:40:54 +0100

    Fix of the user not being rehydrated upon reload

--- apps/client/src/components/AppLayout.tsx
+++ apps/client/src/components/AppLayout.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router";
 import classes from "./AppLayout.module.css";
+import { useRefreshUser } from "../hooks/useRefreshUser.ts";
 
 export const AppLayout = () => {
+  useRefreshUser();
+
   return (
     <div className={classes.background}>
       <Outlet />
