6572a98437be7b845efe3c1f4165ee6026c32b1d
Jakub Hrubant <jakub.hrubant@unicornuniversity.com>
2026-04-03 14:54:48 +0200

    feature:patient-be - code review fixes

--- apps/server/src/modules/patient/dto/create-patient.dto.ts
+++ apps/server/src/modules/patient/dto/create-patient.dto.ts
@@ -1,6 +1,4 @@
 import { createPatientSchema } from "@clinio/shared";
 import { createZodDto } from "nestjs-zod";
 
-console.log("DEBUG: createPatientSchema is:", createPatientSchema); // If this logs 'undefined', Fix #1 is required
-
 export class CreatePatientDto extends createZodDto(createPatientSchema) {}
