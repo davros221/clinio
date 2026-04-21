2b7cf5c8ef6168a57b1c99a5985f36ea1880011d
DZCDJ6J <Lenka Slamova ext.Lenka.Slamova@skoda-auto.cz>
2026-04-21 18:31:03 +0200

    Repair missclick

--- apps/client/src/utils/index.ts
+++ apps/client/src/utils/index.ts
@@ -1,5 +1,5 @@
 export * from "./AuthToken.ts";
-export * from "./confirmModal.ts";
+export * from "./confirmModal.tsx";
 export * from "./error.ts";
 export * from "./notification.ts";
 export * from "./StringUtils.ts";
